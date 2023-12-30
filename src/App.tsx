a5f4b00b177207c45dcfc8b2e00b76249814a368
FranckyPapaTango <rafaralahyf@gmail.com>
2023-12-30 05:19:09 +0100

    -first commit (page d'accueil)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-container">
+      <img src="content/images/Madagascar-Satellite-Map-378x550.png" alt="Madagascar Map" className='groslogo'/>
+<div className='expo'>
+  <h1>Bienvenu</h1>
+  <p>Pour le Commerce Equitable</p>
+  <p>Pour la Promotion des Savoirs-Faire</p>
+  <p>Pour le Respect de la Biodiversité</p>
+</div>
+      </div>
     </div>
   );
 }
